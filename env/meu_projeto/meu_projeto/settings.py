35dd87ab422ef46ec9bac0f86c0ac558fcb68754
Walisson oliveira <walissonoliveira1@outlook.com>
2024-11-27 12:10:18 -0300

    Instalando a biblioteca celery redis para envio de email assíncrona

--- env/meu_projeto/meu_projeto/settings.py
+++ env/meu_projeto/meu_projeto/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,8 +126,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email
+
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
+else: 
+    DEFAULT_FROM_EMAIL = "walissonolievira1@outlook.com"
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = config('EMAIL_USE_TLS')
+    EMAIL_PORT = config('EMAIL_PORT')
+    EMAIL_HOST = config('EMAIL_HOST')
+
+        
 
