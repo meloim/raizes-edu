21acf0720b1573013bcaa80725d0099dacbf340a
Aluno <matheus.eloim@gmail.com>
2024-11-27 14:49:11 -0300

    Ajustes de rota

--- env/meu_projeto/cadastro/urls.py
+++ env/meu_projeto/cadastro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base/', views.base, name='base'),
+    path('', views.base, name='base'),
     path('lista/', views.listar_usuarios, name='listar_usuarios'),
     path('novo/', views.criar_usuario, name='criar_usuario'),
     path('editar/<int:pk>/', views.editar_usuario, name='editar_usuario'),
