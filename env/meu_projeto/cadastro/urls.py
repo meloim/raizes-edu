27d7754302801e468184991482d40bc48134fd66
Walisson oliveira <walissonoliveira1@outlook.com>
2024-11-28 09:54:18 -0300

    branch em contrução

--- env/meu_projeto/cadastro/urls.py
+++ env/meu_projeto/cadastro/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base/', views.base, name='base'),
+    path('', views.base, name='base'),
     path('lista/', views.listar_usuarios, name='listar_usuarios'),
     path('novo/', views.criar_usuario, name='criar_usuario'),
     path('editar/<int:pk>/', views.editar_usuario, name='editar_usuario'),
     path('excluir/<int:pk>/', views.excluir_usuario, name='excluir_usuario'),
-    
+    path('home/',views.homelider, name='home_lider'),
    
 ]
    
